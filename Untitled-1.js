1821a38b0d39fbce3f95fe92f0f2850e670439f4
Aziz <exaple@gmail.com>
2023-06-27 20:45:29 +0600

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,39 @@
+'user strict';
+
+
+//alert('Hello');
+
+//const result = confirm ('Are you here');
+//console.log(result);
+
+//const answer = prompt('Вам есть 18?', '18');
+//console.log(answer);
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя','');
+// answer[1] = prompt('Как ваше фамилия','');
+// answer[2] = prompt('Сколько вам лет','');
+
+// console.log(typeof(answer));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Aziz';
+// alert(`Hello, ${user}`);
+
+
+console.log('arr' + ' - object');
+console.log(4 + + '5');
+
+let incr = 10,
+	dicr = 10;
+
+// incr++;
+// dicr--;
+
+console.log(incr++);
+console.log(dicr--);
